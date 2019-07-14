9c98ce09332214de379468fcc02369a8526cf30e
adrianasalome31 <adrianasalome2017@gmail.com>
2019-07-13 23:23:49 -0600

    first commit

---
+++ WebServicesCategories/DataCategoriesModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebServicesCategories
+{
+    public class DataCategoriesModel
+    {
+
+        public DataSet getCategories()
+        {
+            //EL DataSet REPRESENTA UNA MEMORIA CACHÉ DE DATOS EN MEMORIA 
+            DataSet dataTable = new DataSet();
+
+            //A TRAVEZ DE LA CADENA DE CONEXION DEL WEBCONFIG Y LA OBTENEMOS  
+            //CON EL CONFIGURATIONMANAGER 
+            using (SqlConnection con = new SqlConnection(
+                ConfigurationManager.ConnectionStrings["connectionLocal"]
+                .ConnectionString))
+            {
+                //SE ABRE LA CONEXION 
+                con.Open();
+
+                //SE UTILIZA PARA LLENAR UN DATASET CON LOS ELEMENTOS NECESARIOS  
+                //COMO UNA CONEXION DE BASE DE DATOS 
+                using (SqlDataAdapter sqlAdapter = new SqlDataAdapter(
+                    "SELECT * FROM Categories", con))
+                {
+                    //SE LLENA EL DATASET CON LOS DATOS OBTENIDOS EN EL SQLADAPTER 
+                    sqlAdapter.Fill(dataTable);
+                }
+            }
+            //REGRESAMOS LOS DATOS COMO DATOS EN MEMORIA 
+            return dataTable;
+        }
+    }
+}
---
+++ WebServicesCategories/ServicesCategories.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Services;
+using System.Data;
+using System.Xml.Serialization;
+
+namespace WebServicesCategories
+{
+    /// <summary>
+    /// Descripción breve de ServicesCategories
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class ServicesCategories : System.Web.Services.WebService
+    {
+
+        
+        DataCategoriesModel dataEmployess = new DataCategoriesModel();
+        [WebMethod]
+        public DataSet getCategories()
+        {
+            return dataEmployess.getCategories();
+        }
+    }
+}
